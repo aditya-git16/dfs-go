3d2019795ccf41915939997954d905eacfe4927f
aditya <es1221211@iitd.ac.in>
2024-08-20 18:48:11 +0530

    testing tcp transport

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,9 +1,20 @@
 package p2p
 
-import "net"
+import (
+	"net"
+	"sync"
+)
 
 type TCPTransport struct {
-	ListenAddress string
-	Listener 	  net.Listener
-	peers 		  map[net.Addr]
+	listenAddress string
+	listener 	  net.Listener
+
+	mu 			  sync.RWMutex
+	peers 		  map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		listenAddress: listenAddr,
+	}
 }
